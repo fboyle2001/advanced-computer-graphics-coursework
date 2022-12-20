98abc89003f8f1c72b26a232749e144664c74094
fboyle2001 <finlayboyle2001@gmail.com>
2022-12-20 17:26:44 +0000

    Cleaned up bezier surface python code

--- helpers/parametric_surfaces/bezier_surface.py
+++ helpers/parametric_surfaces/bezier_surface.py
@@ -1,116 +1,69 @@
-from vector import Vector3
+# Mostly derived from lecture slides
+# Surface plotting and general numpy changes to original are from https://stackoverflow.com/q/72154002
 
 import numpy as np
-import math
 import matplotlib.pyplot as plt
-
-us = np.linspace(0, 1, 40)
-vs = np.linspace(0, 1, 40)
+from matplotlib import cm
+import math
 
 def bernstein_basis_polynomial(n, i):
     return lambda t: math.comb(n, i) * (t ** i) * ((1 - t) ** (n - i))
 
-# control_points = [
-#     [
-#         Vector3(0, 0, 0),
-#         Vector3(0, 4, 0),
-#         Vector3(0, 8, -3)
-#     ],
-#     [
-#         Vector3(2, 0, 6),
-#         Vector3(2, 4, 0),
-#         Vector3(2, 8, 0)
-#     ],
-#     [
-#         Vector3(4, 0, 0),
-#         Vector3(4, 4, 0),
-#         Vector3(4, 8, 0)
-#     ],
-#     [
-#         Vector3(6, 0, 0),
-#         Vector3(6, 4, -3),
-#         Vector3(6, 8, 0)
-#     ],
-# ]
+def generate_bezier_surface(control_points, samples):
+    steps = np.linspace(0, 1, samples)
 
-# control_points = [
-#     [
-#         Vector3(0, 0, 0),
-#         Vector3(2, 0, 6),
-#         Vector3(4, 0, 0),
-#         Vector3(6, 0, 0)
-#     ],
-#     [
-#         Vector3(0, 4, 0),
-#         Vector3(2, 4, 0),
-#         Vector3(4, 4, 0),
-#         Vector3(6, 4, -3)
-#     ],
-#     [
-#         Vector3(0, 8, -3),
-#         Vector3(2, 8, 0),
-#         Vector3(4, 8, 0),
-#         Vector3(6, 8, 0)
-#     ]
-# ]
-
-control_points = [
-    [Vector3(1.8, -0.3, 0.), Vector3(1.8, 0.13, 0.1), Vector3(1.8, 0.5, 0.)],
-    [Vector3(2., -0.3, 0.06), Vector3(2.1, 0.1, 0.1), Vector3(2.1, 0.5, 0.1)],
-    [Vector3(2.3, -0.3, 0.1), Vector3(2.3, 0.13, 0.2), Vector3(2.3, 0.5, 0.1)],
-    [Vector3(2.4, -0.3, 0.1), Vector3(2.5, 0.1, 0.15), Vector3(2.5, 0.5, 0.1)],
-    [Vector3(2.6, -0.3, 0.), Vector3(2.6, 0.1, 0.1), Vector3(2.5, 0.5, 0.)]
-]
-
-m = len(control_points)
-m_basis = []
-print("u deg", m - 1)
-
-for i in range(m):
-    m_basis.append(bernstein_basis_polynomial(m - 1, i))
-
-n = len(control_points[0])
-print("v deg", n - 1)
-n_basis = []
-
-for j in range(n):
-    n_basis.append(bernstein_basis_polynomial(n - 1, j))
-
-def p(u, v):
-    point = Vector3(0, 0, 0)
+    # Pregenerate the basis polynomials
+    m = len(control_points)
+    m_basis = []
 
     for i in range(m):
-        for j in range(n):
-            c = control_points[i][j]
-            point += c.scale(m_basis[i](u) * n_basis[j](v))
+        m_basis.append(bernstein_basis_polynomial(m - 1, i))
+
+    n = len(control_points[0])
+    n_basis = []
+
+    for j in range(n):
+        n_basis.append(bernstein_basis_polynomial(n - 1, j))
     
-    return point
+    points = []
 
-xs = []
-ys = []
-zs = []
+    for u in steps:
+        fixed_u_points = []
 
-for u in us:
-    for v in vs:
-        uvd = p(u, v)
+        for v in steps:
+            point = [0, 0, 0]
 
-        xs.append(uvd.x)
-        ys.append(uvd.y)
-        zs.append(uvd.z)
+            for i in range(m):
+                for j in range(n):
+                    control = control_points[i][j]
+                    uv_scale = m_basis[i](u) * n_basis[j](v)
 
-fig = plt.figure()
-ax = fig.add_subplot(projection="3d")
-ax.scatter(xs, ys, zs)
+                    point[0] += control[0] * uv_scale
+                    point[1] += control[1] * uv_scale
+                    point[2] += control[2] * uv_scale
+            
+            fixed_u_points.append(point)
+        
+        points.append(fixed_u_points)
+    
+    return np.array(points)
 
-bxs = []
-bys = []
-bzs = []
+def plot_surface(surface_points):
+    fig = plt.figure()
+    ax = fig.add_subplot(projection="3d")
+    x, y, z = surface_points.T
+    ax.plot_surface(x, y, z, cmap=cm.gray, linewidth=1, antialiased=False) # type: ignore
+    plt.show()
 
-for cp in control_points:
-    for b in cp:
-        bxs.append(b.x)
-        bys.append(b.y)
-        bzs.append(b.z)
+control_points = np.array(
+    [
+        [[1.8, -0.3, 0.], [1.8, 0.13, 0.1], [1.8, 0.5, 0.]],
+        [[2., -0.3, 0.06], [2.1, 0.1, 0.1], [2.1, 0.5, 0.1]],
+        [[2.3, -0.3, 0.1], [2.3, 0.13, 0.2], [2.3, 0.5, 0.1]],
+        [[2.4, -0.3, 0.1], [2.5, 0.1, 0.15], [2.5, 0.5, 0.1]],
+        [[2.6, -0.3, 0.], [2.6, 0.1, 0.1], [2.5, 0.5, 0.]]
+    ]
+)
 
-ax.scatter(bxs, bys, bzs, color="red")
-plt.show()
+surface = generate_bezier_surface(control_points, 40)
+plot_surface(surface)
